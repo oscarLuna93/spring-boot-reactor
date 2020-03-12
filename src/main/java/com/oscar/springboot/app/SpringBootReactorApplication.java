cc0f2360cf4969cbb0943bfa8a08cc54c19463f2
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-12 12:08:56 -0600

    creando flujo

--- src/main/java/com/oscar/springboot/app/SpringBootReactorApplication.java
+++ src/main/java/com/oscar/springboot/app/SpringBootReactorApplication.java
@@ -1,13 +1,24 @@
 package com.oscar.springboot.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Oscar", "Ary", "Miguel", "Omar")
+				.doOnNext(System.out::println);
+		
+		nombres.subscribe();
 	}
 
 }
