61c9eb86db67314935ef2a4530ecdd4bbff458df
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-12 12:17:56 -0600

    manejo de error en suscribe

--- src/main/java/com/oscar/springboot/app/SpringBootReactorApplication.java
+++ src/main/java/com/oscar/springboot/app/SpringBootReactorApplication.java
@@ -1,5 +1,7 @@
 package com.oscar.springboot.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,16 +11,24 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner{
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Oscar", "Ary", "Miguel", "Omar")
-				.doOnNext(System.out::println);
+		Flux<String> nombres = Flux.just("Oscar", "Ary", "", "Miguel", "Omar")
+				.doOnNext(e -> {
+					if(e.isEmpty()) {
+						throw new RuntimeException("nombre no puede ser vacio");
+					}
+					System.out.println(e);
+				});
 		
-		nombres.subscribe();
+		nombres.subscribe(e -> log.info(e),
+				error -> log.error(error.getMessage()));
 	}
 
 }
