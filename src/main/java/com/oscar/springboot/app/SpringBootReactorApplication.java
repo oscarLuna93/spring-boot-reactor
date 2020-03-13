c20ade52c59ff8fed272d2cfebefee6af7f50f6d
Oscar Luna <oscarluna.dev93@gmail.com>
2020-03-12 22:31:02 -0600

    Flux apartir de Iterable

--- src/main/java/com/oscar/springboot/app/SpringBootReactorApplication.java
+++ src/main/java/com/oscar/springboot/app/SpringBootReactorApplication.java
@@ -1,5 +1,8 @@
 package com.oscar.springboot.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -21,7 +24,16 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Oscar Hernandez", "Ary Luna", "Miguel Luna", "Omar Cruz", "Maria Sabina", "Diego Luna", "Bruce lee");
+		List<String> usuariosList = new ArrayList<String>();
+		usuariosList.add("Oscar Hernandez");
+		usuariosList.add("Ary Luna");
+		usuariosList.add("Miguel Luna");
+		usuariosList.add("Omar Cruz");
+		usuariosList.add("Maria Sabina");
+		usuariosList.add("Diego Luna");
+		usuariosList.add("Bruce lee");
+		
+		Flux<String> nombres = Flux.fromIterable(usuariosList);//Flux.just("Oscar Hernandez", "Ary Luna", "Miguel Luna", "Omar Cruz", "Maria Sabina", "Diego Luna", "Bruce lee");
 		
 		Flux<Usuario> usuarios = nombres.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
 				.filter(usuario -> usuario.getNombre().equalsIgnoreCase("Bruce"))
